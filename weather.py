4b7dd57efe5e2b4acc7c2f2c21ee3ae6aa489f91
Kyle Hines <hines.kyle.m@gmail.com>
2016-04-25 18:08:22 -0600

    updated API call

--- weather.py
+++ weather.py
@@ -1,7 +1,11 @@
 import requests
 import json
 
-r = requests.get('https://api.forecast.io/forecast/61707aa3bf5d6b50275f912d1e8d6888/40.1651814,-105.8572453')
+API_KEY = "61707aa3bf5d6b50275f912d1e8d6888"
+LAT = 40.1651814
+LONG = -105.8572453
+
+r = requests.get('https://api.forecast.io/forecast/%s/%d,%d' % (API_KEY, LAT, LONG))
 parsed_json = r.json()
 
 print parsed_json["timezone"]
