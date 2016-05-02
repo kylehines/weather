8db37bfb1ec157e1d6600c3a435533004b217e40
Kyle Hines <hines.kyle.m@gmail.com>
2016-05-02 14:21:09 -0600

    littlebitclosernow

---
+++ send_email.py
@@ -0,0 +1,43 @@
+import base64
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mimetypes
+import os
+
+from apiclient import errors
+
+client_secret = os.path(khines/desktop/weather/client_secret.json)
+my_gmail = "hines.kyle.@gmail.com"
+
+def SendMessage(service, user_id, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print 'Message Id: %s' % message['id']
+    return message
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+def CreateMessage(sender, to, subject, message_text):
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.b64encode(message.as_string())}
+
+new_message = CreateMessage(my_gmail,my_gmail,"test","This is a test of hte emergency snuggle system!")
+SendMessage(client_secret.users(), my_gmail, new_message)
